ee306084185c885498208d76a460af9f07561927
Agrim <agrimarya18@gmail.com>
2026-04-20 01:08:31 +0530

    removing files

--- server/models/Course.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const courseSchema = new mongoose.Schema({
-  name: { type: String, required: true }, // e.g. B.Tech Computer Science
-  duration_years: { type: Number, required: true },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Course', courseSchema);
